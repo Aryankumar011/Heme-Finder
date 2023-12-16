c6b6611f725e4837f2a13ef276d2e04324b92e05
Aryankumar011 <CSE_63@gecpat.gujgov.edu.in>
2023-12-16 16:43:56 +0530

    Login Page Done

--- lib/constants/theme.dart
+++ lib/constants/theme.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
 ThemeData themeData = ThemeData(
+    primaryColor: Colors.red,
     scaffoldBackgroundColor: Colors.white,
+    inputDecorationTheme: InputDecorationTheme(
+      border: outlineInputBorder,
+      errorBorder: outlineInputBorder,
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      disabledBorder: outlineInputBorder,
+      prefixIconColor: Colors.grey,
+      suffixIconColor: Colors.grey,
+    ),
     elevatedButtonTheme: ElevatedButtonThemeData(
         style: ElevatedButton.styleFrom(
             backgroundColor: Colors.red,
@@ -9,3 +19,6 @@ ThemeData themeData = ThemeData(
             textStyle: TextStyle(
               fontSize: 18,
             ))));
+
+OutlineInputBorder outlineInputBorder =
+    OutlineInputBorder(borderSide: BorderSide(color: Colors.grey));
--- lib/screens/auth_ui/login/login.dart
+++ lib/screens/auth_ui/login/login.dart
@@ -1,20 +1,71 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:home_finder_new/widgets/primarybutton/primary_button.dart';
 import 'package:home_finder_new/widgets/top_titles/top_titleles.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({super.key});
 
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  bool isShowPassword = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // appBar: AppBar(),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // Icon(Icons.arrow_back),
-          TopTitles(
-            subTitle: "Wellcome Back To Home Finder App",
+          const TopTitles(
+            subTitle: "Welcome Back To Home Finder App",
             title: "Login",
           ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.mail), hintText: "Email"),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.password),
+                    suffixIcon: CupertinoButton(
+                        child: Icon(
+                          Icons.visibility,
+                          color: Colors.grey,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            isShowPassword = !isShowPassword;
+                          });
+                        }),
+                    hintText: "Password"),
+                obscureText: isShowPassword,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              PrimaryButton(onPressed: () {}, title: "Login"),
+            ]),
+          ),
+          const Center(child: Text("Don't have an Account ?")),
+          SizedBox(
+            height: 12,
+          ),
+
+          Center(
+              child: CupertinoButton(
+                  child: Text('Create an Account'), onPressed: () {})),
+
+          // TextButton(onPressed: () {}, child: Text('Create an Account')),
         ],
       ),
     );
